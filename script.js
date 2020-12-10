33dc524512d79f9c0208a9223f42a20cfc5713ad
Schulzkafer <gatsby886@gmail.com>
2020-12-10 16:44:06 -0300

    'message'

--- script.js
+++ script.js
@@ -174,95 +174,114 @@ let victoryArr = [];
 
 function victoryLine (numbOfLine, numbOfcells, necessaryClass) {
 
-victoryArr.push([numbOfLine, numbOfcells]);
+	victoryArr.push([numbOfLine, numbOfcells, necessaryClass]);
 
-if (chosenLine == 1) {
-  victoryArr = [];
-  
-	if (numbOfLine) {
-		allLinesButtons.map(x=>x.setAttribute('disabled','disabled'));
-		document.querySelector(numbOfLine).style.visibility = 'visible';
-		showPrize(numbOfcells, necessaryClass);
-	bigButton.value = 'TAKE';
-}
-} else if (chosenLine == 3) {
+	if (chosenLine == 1) {
+		victoryArr = [];
+
+		if (numbOfLine) {
+			allLinesButtons.map(x=>x.setAttribute('disabled','disabled'));
+			document.querySelector(numbOfLine).style.visibility = 'visible';
+			showPrize(numbOfcells, necessaryClass);
+			bigButton.value = 'TAKE';
+		}
+	} else if (chosenLine == 3) {
 if (victoryArr.length == 3) {  //////////////////  && если есть хотя бы одно зачение не нулевое, то отправляем на подсчет. подсчет можно производить прямо из drawlines, то есть из drawlines передавать значения в нужное время в showPrize
+	
 	drawLines();
 }
 } 
 }
 
+
+let winSum = 0;
 function showPrize(numbOfcells, necessaryClass) {
-	
-	let winSum = 0;
-switch (necessaryClass) {
-case 'cell-php':
-if (numbOfcells == 3) {
-	winSum = 2;
-} else if (numbOfcells == 4) {
-	winSum = 3;
-} else if (numbOfcells == 5) {
-	winSum = 10;
-}
-break;
-case 'cell-ruby':
-if (numbOfcells == 3) {
-	winSum = 3;
-} else if (numbOfcells == 4) {
-	winSum = 5;
-} else if (numbOfcells == 5) {
-	winSum = 20;
-}
-break;
-case 'cell-swift':
-if (numbOfcells == 3) {
-	winSum = 5;
-} else if (numbOfcells == 4) {
-	winSum = 10;
-} else if (numbOfcells == 5) {
-	winSum = 50;
-}
-break;
-case 'cell-java':
-if (numbOfcells == 3) {
-	winSum = 10;
-} else if (numbOfcells == 4) {
-	winSum = 30;
-} else if (numbOfcells == 5) {
-	winSum = 100;
-}
-break;
-}
-winSum = winSum * (betNumber.innerHTML / chosenLine);
-document.querySelector('#lines-word').innerHTML = "PRIZE";
-document.querySelector('#lines-number').innerHTML = winSum;
+
+	switch (necessaryClass) {
+		case 'cell-php':
+		if (numbOfcells == 3) {
+			winSum += 2;
+		} else if (numbOfcells == 4) {
+			winSum += 3;
+		} else if (numbOfcells == 5) {
+			winSum += 10;
+		}
+		break;
+		case 'cell-ruby':
+		if (numbOfcells == 3) {
+			winSum += 3;
+		} else if (numbOfcells == 4) {
+			winSum += 5;
+		} else if (numbOfcells == 5) {
+			winSum += 20;
+		}
+		break;
+		case 'cell-swift':
+		if (numbOfcells == 3) {
+			winSum += 5;
+		} else if (numbOfcells == 4) {
+			winSum += 10;
+		} else if (numbOfcells == 5) {
+			winSum += 50;
+		}
+		break;
+		case 'cell-java':
+		if (numbOfcells == 3) {
+			winSum += 10;
+		} else if (numbOfcells == 4) {
+			winSum += 30;
+		} else if (numbOfcells == 5) {
+			winSum += 100;
+		}
+		break;
+	}
+	let tempSum = winSum * (betNumber.innerHTML / chosenLine);
+	document.querySelector('#lines-word').innerHTML = "PRIZE";
+	document.querySelector('#lines-number').innerHTML = tempSum;
 }
 
 
 function drawLines () {
-		let timeWait = 300;
-if (victoryArr[0][0]) {
-	setTimeout(()=>document.querySelector(victoryArr[0][0]).style.visibility = 'visible', timeWait);
-	timeWait+=300;
-} 
- if (victoryArr[1][0]) {
-	setTimeout(()=>document.querySelector(victoryArr[1][0]).style.visibility = 'visible', timeWait);
-	timeWait+=300;
-} 
-if (victoryArr[2][0]) {
-		setTimeout(()=>document.querySelector(victoryArr[2][0]).style.visibility = 'visible', timeWait);
-	timeWait+=300;
-}
-bigButton.value = 'TAKE';
-victoryArr = [];
+	bigButton.setAttribute("disabled", "disabled");
+	allLinesButtons.map(x=>x.setAttribute('disabled', "disabled"));
+	let timeWait = 200;
+	if (victoryArr[0][0]) {
+		setTimeout(()=>{document.querySelector(victoryArr[0][0]).style.visibility = 'visible';
+			showPrize(victoryArr[0][1], victoryArr[0][2])
+		}, timeWait);
+		timeWait+=200;
+	} 
+	if (victoryArr[1][0]) {
+		setTimeout(()=>{
+			document.querySelector(victoryArr[1][0]).style.visibility = 'visible';
+			showPrize(victoryArr[1][1], victoryArr[1][2])
+		}, timeWait);
+		timeWait+=200;
+	} 
+	if (victoryArr[2][0]) {
+		setTimeout(()=>{document.querySelector(victoryArr[2][0]).style.visibility = 'visible';
+			showPrize(victoryArr[2][1], victoryArr[2][2])}, timeWait);
+		timeWait+=200;
+	}
+	
+	setTimeout(()=>{
+		if (timeWait > 200) {
+			bigButton.value = 'TAKE';
+		} 
+		bigButton.removeAttribute("disabled");
+		allLinesButtons.map(x=>x.removeAttribute('disabled'));
+		victoryArr = [];
+	}, timeWait+10);
+
 }
 
 function calculate() {
 	allLinesButtons.map(x=>x.removeAttribute('disabled'));
 	bigButton.value = 'START';
+	winSum = 0;
 	creditNumber.innerHTML = +creditNumber.innerHTML + +document.querySelector('#lines-number').innerHTML;
 	document.querySelector('#lines-word').innerHTML = "LINES";
-document.querySelector('#lines-number').innerHTML = chosenLine;
+	document.querySelector('#lines-number').innerHTML = chosenLine;
 
 	Array.from(allLines).map(x=>x.style.visibility = "hidden");
 }
@@ -286,10 +305,10 @@ let chosenLine = 3;
 document.getElementById('lines-number').innerHTML = 3;
 
 function chooseLine (event) {
-		if (creditNumber.innerHTML >= 3 && threeLinesButton.hasAttribute("disabled"))  {
-			threeLinesButton.removeAttribute("disabled", "disabled");
+	if (creditNumber.innerHTML >= 3 && threeLinesButton.hasAttribute("disabled"))  {
+		threeLinesButton.removeAttribute("disabled", "disabled");
 		bigButton.removeAttribute("disabled", "disabled");
-}
+	}
 
 	if (hideLinesTimer) clearTimeout(hideLinesTimer);
 	(Array.from(allLines)).map(x=>x.style.visibility = 'hidden');
@@ -317,8 +336,22 @@ let betNumber = document.querySelector('#bet-number');
 creditNumber.innerHTML = 100;
 
 let betPoint = 1;
+
 function showBet() {
 	betNumber.innerHTML = chosenLine * betPoint;
+	if (+betNumber.innerHTML > +creditNumber.innerHTML) {
+		if (+chosenLine > +creditNumber.innerHTML && chosenLine > 1) {
+			chosenLine -=2;
+		document.getElementById('lines-number').innerHTML -= 2;
+		threeLinesButton.setAttribute("disabled", "disabled");
+	}
+	betPoint = 1;
+	showBet();
+	} else {
+		bigButton.removeAttribute("disabled", "disabled");
+	}
+
+
 }
 showBet();
 
@@ -326,23 +359,28 @@ let betButton = document.querySelector('#bet-button');
 betButton.addEventListener('click', moreBet);
 
 function moreBet () {
-if (creditNumber.innerHTML >= 3 && threeLinesButton.hasAttribute("disabled"))  {
-			threeLinesButton.removeAttribute("disabled", "disabled");
+	if (creditNumber.innerHTML >= 3 && threeLinesButton.hasAttribute("disabled"))  {
+		threeLinesButton.removeAttribute("disabled", "disabled");
 		bigButton.removeAttribute("disabled", "disabled");
-}
+	}
+
+	// if (creditNumber.innerHTML >= 1 && document.querySelector('#lines-number').innerHTML == 1 && bigButton.hasAttribute("disabled"))  {
+	// 	//oneLineButton.removeAttribute("disabled", "disabled");
+	// 	bigButton.removeAttribute("disabled", "disabled");
+	// }
 
 
-if (chosenLine * betPoint > creditNumber.innerHTML)   
+	if (chosenLine * betPoint > creditNumber.innerHTML)   
 		console.log(chosenLine)
 	console.log(betPoint)
 
 	if (betPoint < 10) {
 	//if (!checkBetForOne(betNumber.innerHTML,1)) break;
 	betPoint = +betPoint + 1;
-	} else if (betPoint < 50) {
+} else if (betPoint < 50) {
 		//if (!checkBetForOne(betNumber.innerHTML,5)) break;
 		betPoint = +betPoint + 5;
-		} else if (betPoint < 90) {
+	} else if (betPoint < 90) {
 	//	if (!checkBetForOne(betNumber.innerHTML,10)) break;
 	betPoint = +betPoint + 10;
 } else {
